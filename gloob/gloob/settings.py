f2df9faa23cae5867328d00ba9beb2d4bc19930c
shajeer <shajeer123@gmail.com>
2026-02-02 15:10:42 +0400

    Update ALLOWED_HOSTS for PythonAnywhere

--- gloob/gloob/settings.py
+++ gloob/gloob/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-31at#s2g4)$!o6$*1ku4*krislj#rd%22b$&%i@(qd$7x98o=s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['shajeer3137.pythonanywhere.com']
+ALLOWED_HOSTS = ['shajeer3137.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 
