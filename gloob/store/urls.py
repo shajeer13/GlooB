bb525f2bc85c9ee30bfcac5c740d335646a30bb1
Muhammed Shajeer <muhammedshajeer12@gmail.come>
2026-02-08 22:50:03 +0000

    Save local changes before pulling

--- gloob/store/urls.py
+++ gloob/store/urls.py
@@ -1,12 +1,6 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', auth_views.LoginView.as_view(template_name='store/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('signup/', views.signup, name='signup'),
-    path('add-to-cart/<int:id>/', views.add_to_cart, name='add_to_cart'),
-    path('cart/', views.cart, name='cart'),
 ]
