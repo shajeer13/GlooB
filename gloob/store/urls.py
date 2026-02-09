145650c13f206b49a11e0ab94f5e93a1f84e8913
shajeer <shajeer123@gmail.com>
2026-02-10 03:12:56 +0400

    Updated URLs: home, login, signup, cart, add-to-cart; resolved merge conflicts

--- gloob/store/urls.py
+++ gloob/store/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('cart/', views.cart, name='cart'),  # ഇങ്ങനെ ഉണ്ടെങ്കിൽ
+    path('add-to-cart/<int:id>/', views.add_to_cart, name='add_to_cart'),
 ]
